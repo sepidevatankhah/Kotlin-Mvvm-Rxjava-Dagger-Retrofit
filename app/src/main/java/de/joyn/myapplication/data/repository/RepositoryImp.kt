babcb7bbd55d30fbc2078a3305a3b38d3d9b4780
Sepide <sun.vatankhah@gmail.com>
2019-08-04 11:46:56 +0200

    Revert "Add Paging DataSource and DataSourceFactory"
    This reverts commit 26299d7c

--- app/src/main/java/de/joyn/myapplication/data/repository/RepositoryImp.kt
+++ app/src/main/java/de/joyn/myapplication/data/repository/RepositoryImp.kt
@@ -2,16 +2,22 @@ package de.joyn.myapplication.data.repository
 
 import de.joyn.myapplication.domain.repository.Repository
 import de.joyn.myapplication.network.RestApi
-import de.joyn.myapplication.network.dto.BaseModel
 import de.joyn.myapplication.network.dto.Models
-import io.reactivex.Flowable
 import io.reactivex.Single
 import timber.log.Timber
 import javax.inject.Inject
 
 class RepositoryImp @Inject constructor(private val restApi: RestApi) : Repository {
-    override fun getFlowerUseCase(query: String?): Flowable<BaseModel> {
-        return restApi.getPhotos(query)
+    override fun getFlowerUseCase(query: String?): Single<List<Models.FlowerResponse>> {
+        return Single.create { emitter ->
+            restApi.getFlowers(query).subscribe({ response ->
+                Timber.d("response : " + response.response.size)
+                emitter.onSuccess(response.response)
+            }, {
+                emitter.onError(it)
+            })
+        }
     }
+
 
 }
