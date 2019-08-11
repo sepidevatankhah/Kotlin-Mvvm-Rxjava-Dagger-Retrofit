12c07baa34eec9b3fa3fe78654c72b3274878212
Sepide <sun.vatankhah@gmail.com>
2019-08-11 16:30:16 +0200

    -

---
+++ app/src/main/java/de/joyn/myapplication/ui/base/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package de.joyn.myapplication.ui.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel<M> : ViewModel() {
+
+    var stateLiveData: LiveData<M> = MutableLiveData()
+
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+        super.onCleared()
+    }
+}
